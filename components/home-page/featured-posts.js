6a982aa631a17d6afe2593ad25701699c8738e58
Steven Martinez <esteven123123@outlook.com>
2023-06-27 00:11:24 -0400

    Resolve Conflict

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -4,11 +4,7 @@ import { PostsGrids } from "../posts/posts-grid";
 const FeaturedPost = (props) => {
   return (
     <section className={classes.latest}>
-<<<<<<< HEAD
-      <h2>Publicaciones Destacadas</h2>
-=======
       <h2>Publicaciones</h2>
->>>>>>> 6785166f0f785674c7bea5b7b8434d8a07671d27
       <PostsGrids posts={props.posts} />
     </section>
   );
